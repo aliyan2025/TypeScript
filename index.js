06f8ccf00894988143366eaef4bca6c3d3ceac9c
aliyan2025 <ridhoaliyan3@gmail.com>
2026-04-15 08:20:30 +0700

    secound commit

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let nama = "Budi";
 let umur = 17;
 let aktif = true;
 console.log(nama, umur, aktif);
+const siswa1 = {
+    id: 1,
+    nama: "Aliyan Ridho",
+    kelas: "XI RPL 1"
+};
+console.log(siswa1);
+function hitungUmur(tahunLahir) {
+    const tahunSekarang = new Date().getFullYear();
+    const umur = tahunSekarang - tahunLahir;
+    return umur;
+}
+const umurSaya = hitungUmur(2008);
+console.log("Umur:", umurSaya);
--- index.ts
+++ index.ts
@@ -2,3 +2,31 @@ let nama: string = "Budi";
 let umur: number = 17; 
 let aktif: boolean = true; 
 console.log(nama, umur, aktif);
+
+interface Siswa {
+  id: number;
+  nama: string;
+  kelas: string;
+}
+
+
+const siswa1: Siswa = {
+  id: 1,
+  nama: "Aliyan Ridho",
+  kelas: "XI RPL 1"
+};
+
+
+console.log(siswa1);
+
+function hitungUmur(tahunLahir: number): number {
+  const tahunSekarang = new Date().getFullYear();
+  const umur = tahunSekarang - tahunLahir;
+  return umur;
+}
+
+
+const umurSaya = hitungUmur(2008);
+
+
+console.log("Umur:", umurSaya);
